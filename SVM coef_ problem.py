b089ce80b48bb1518a4dcfdc236f29e2d6d53bdd
Jingwen Li <66143676+JingwenLi173@users.noreply.github.com>
2021-11-02 12:15:27 -0400

    Update SVM coef_ problem.py

--- SVM coef_ problem.py
+++ SVM coef_ problem.py
@@ -14,3 +14,8 @@ print('Support vectors = ', clf.support_vectors_)
 print('Number of support vectors for each class = ', clf.n_support_)
 print('Coefficients of the support vector in the decision function = ', np.abs(clf.dual_coef_))
 
+# https://stats.stackexchange.com/questions/39243/how-does-one-interpret-svm-feature-weights
+
+# Thus, the coef_ is just the weights "w" of the hyperplane "wTx+b=0" when distance between support vector and hyperplane equals 1. 
+# The reason we use weights here as importance value is very similar to linear regression.  
+# But this is only for linear kernel.
